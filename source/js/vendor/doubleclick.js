9a264cefb6f0dea2df5372b7b9ad621fd7b88f92
Thiago de Bastos <thiago@thiagodebastos.com>
2016-04-05 15:40:50 +1000

    move animationControls with banner expansion

--- source/js/vendor/doubleclick.js
+++ source/js/vendor/doubleclick.js
@@ -1,34 +1,151 @@
-window.onload = function() {
-	console.log('[custom] Window Loaded');
-	if (Enabler.isInitialized()) {
-		enablerInitHandler();
-	} else {
-		Enabler.addEventListener(studio.events.StudioEvent.INIT, enablerInitHandler);
-	}
-}
-function enablerInitHandler() {
-	console.log('[custom] DCS Enabler Init');
-	if (Enabler.isPageLoaded()) {
-		pageLoadedHandler();
-	} else {
-		Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, pageLoadedHandler);
-	}
-}
-function pageLoadedHandler() {
-	console.log('[custom] Page Loaded');
-	if (Enabler.isVisible()) {
-		adVisibilityHandler();
-	} else {
-		Enabler.addEventListener(studio.events.StudioEvent.VISIBLE, adVisibilityHandler);
-	}
-}
-function adVisibilityHandler() {
-	console.log('[custom] Ad Visible');
-	document.getElementById('animation').addEventListener('click', bgExitHandler, false);
+const creative = {};
 
-    animation();
+/**
+ * Window onload handler.
+ */
+function preInit() {
+  setupDom();
+
+  if (Enabler.isInitialized()) {
+    init();
+  } else {
+    Enabler.addEventListener(
+      studio.events.StudioEvent.INIT,
+      init
+    );
+  }
 }
 
-function bgExitHandler(e, tl) {
-    Enabler.exit('Background Exit');
+/**
+ * Initializes the ad components
+ */
+function setupDom() {
+  creative.dom = {};
+  creative.dom.mainContainer = document.getElementById('animation');
+  creative.dom.expandedExit = document.getElementById('expanded-exit');
+  creative.dom.expandedContent = document.getElementById('expanded-content');
+  creative.dom.collapsedExit = document.getElementById('collapsed-exit');
+  creative.dom.collapsedContent = document.getElementById('collapsed-content');
+  creative.dom.collapseButton = document.getElementById('collapse-button');
+  creative.dom.expandButton = document.getElementById('expand-button');
+  creative.dom.animationControls = document.getElementById('animationControls');
+  // creative.dom.image0 = document.getElementById('main-img-0');
+  // creative.dom.image1 = document.getElementById('main-img-1');
 }
+
+/**
+ * Ad initialisation.
+ */
+function init(e, tl) {
+  Enabler.setStartExpanded(false);
+
+  addListeners();
+
+  // Polite loading
+  if (Enabler.isPageLoaded()) {
+    show();
+  }
+  else {
+    Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, show);
+  }
+	animation();
+}
+
+/**
+ * Adds appropriate listeners at initialization time
+ */
+function addListeners() {
+  Enabler.addEventListener(studio.events.StudioEvent.EXPAND_START, expandStartHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.EXPAND_FINISH, expandFinishHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_START, collapseStartHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_FINISH, collapseFinishHandler);
+  creative.dom.expandButton.addEventListener('click', onExpandHandler, false);
+  creative.dom.collapseButton.addEventListener('click', onCollapseClickHandler, false);
+  creative.dom.expandedExit.addEventListener('click', exitClickHandler);
+  creative.dom.collapsedExit.addEventListener('click', collapsedExitClickHandler);
+}
+
+/**
+ *  Shows the ad.
+ */
+function show() {
+  creative.dom.expandedContent.style.display = 'none';
+  creative.dom.expandedExit.style.display = 'none';
+  creative.dom.collapseButton.style.display = 'none';
+
+  creative.dom.collapsedContent.style.display = 'block';
+  creative.dom.collapsedExit.style.display = 'block';
+  creative.dom.expandButton.style.display = 'block';
+  // creative.dom.image0.style.visibility  = 'visible';
+  // creative.dom.image1.style.visibility  = 'visible';
+}
+
+// ---------------------------------------------------------------------------------
+// MAIN
+// ---------------------------------------------------------------------------------
+
+
+function expandStartHandler() {
+  // Show expand over the top
+  creative.dom.expandedContent.style.display = 'block';
+  creative.dom.expandedExit.style.display = 'block';
+  creative.dom.collapseButton.style.display = 'block';
+  creative.dom.mainContainer.style.width = '970px';
+  creative.dom.mainContainer.style.height = '500px';
+  creative.dom.collapsedContent.style.display = 'none';
+  creative.dom.collapsedExit.style.display = 'none';
+  creative.dom.expandButton.style.display = 'none';
+
+  Enabler.finishExpand();
+}
+
+function expandFinishHandler() {
+  creative.isExpanded = true;
+  creative.dom.animationControls.style.top= "500px";
+}
+
+function collapseStartHandler() {
+  // Perform collapse animation.
+  creative.dom.expandedContent.style.display = 'none';
+  creative.dom.expandedExit.style.display = 'none';
+  creative.dom.collapseButton.style.display = 'none';
+  creative.dom.mainContainer.style.width = '970px';
+  creative.dom.mainContainer.style.height = '250px';
+  creative.dom.collapsedContent.style.display = 'block';
+  creative.dom.collapsedExit.style.display = 'block';
+  creative.dom.expandButton.style.display = 'block';
+
+  // When animation finished must call
+  Enabler.finishCollapse();
+}
+
+function collapseFinishHandler() {
+  creative.isExpanded = false;
+  creative.dom.animationControls.style.top= "250px";
+}
+
+function onCollapseClickHandler(){
+  Enabler.requestCollapse();
+  Enabler.stopTimer('Panel Expansion');
+}
+
+function onExpandHandler(){
+  Enabler.requestExpand();
+  Enabler.startTimer('Panel Expansion');
+}
+
+function exitClickHandler() {
+  Enabler.requestCollapse();
+  Enabler.stopTimer('Panel Expansion');
+  Enabler.exit('BackgroundExit');
+}
+
+function collapsedExitClickHandler() {
+  Enabler.exit('CollapsedExit');
+}
+
+
+/**
+ *  Main onload handler
+ */
+window.addEventListener('load', preInit);
